282f6653597f975847b5ff99bf39fea384096556
Casey Merkey <casey.merkey@gmail.com>
2025-12-02 12:47:17 -0500

    Day 2 Part 2

--- day02/main.go
+++ day02/main.go
@@ -44,13 +44,19 @@ func main() {
 	fmt.Printf("Part 1: %d\n", result)
 	executionTime := float32(time.Since(startTime).Milliseconds()) / float32(1000)
 	fmt.Printf("Completed Part 1 in %f seconds\n\n", executionTime)
+
+	startTime = time.Now()
+	result = part2(ranges)
+	fmt.Printf("Part 2: %d\n", result)
+	executionTime = float32(time.Since(startTime).Milliseconds()) / float32(1000)
+	fmt.Printf("Completed Part 2 in %f seconds\n", executionTime)
 }
 
 func part1(ranges []idRange) int {
 	total := 0
 	for _, rng := range ranges {
-		results := invalidIdsInRange(rng)
-		fmt.Printf("%s-%s:  %v\n", rng.min, rng.max, results)
+		results := invalidIdsInRangePart(rng, 2)
+		// fmt.Printf("%s-%s:  %v\n", rng.min, rng.max, results)
 		for _, id := range results {
 			total += id
 		}
@@ -59,44 +65,62 @@ func part1(ranges []idRange) int {
 	return total
 }
 
-func invalidIdsInRange(rng idRange) []int {
+func part2(ranges []idRange) int {
+	total := 0
+	for _, rng := range ranges {
+		tmpIds := make(map[int]bool)
+		for k := 2; k <= len(rng.max); k++ {
+			results := invalidIdsInRangePart(rng, k)
+			for _, id := range results {
+				if !tmpIds[id] {
+					total += id
+					tmpIds[id] = true
+				}
+			}
+		}
+		// fmt.Printf("%s-%s: %v\n", rng.min, rng.max, tmpIds)
+	}
+	return total
+}
+
+func invalidIdsInRangePart(rng idRange, chunks int) []int {
 
 	ids := make([]int, 0)
-	// ids must be an even number of characters long
 	n := len(rng.min)
 	x := len(rng.max)
 	d := x - n
 
-	// if both min & max are the same, odd number of digits, there are no
-	// invalid ids in range
-	if d == 0 && (n%2 != 0) {
+	// if both min & max are the same number of digits, but not divisible by # of chunks
+	// there are no invalid ids in range
+	if d == 0 && (n%chunks != 0) {
 		return []int{}
 	}
 
-	// after the above test condition, one or both of min & max are even
+	// after the above test condition, one or both of min & max are an appropriate
 	// number of digits
 	min := rng.min
 	max := rng.max
-	// if the min is an odd number of digits, bump it up to the lowest n+1 digit value
-	if n%2 != 0 {
+	// if the min is not evenly divisible by chunks, bump it up to the lowest n+1 digit value
+	if n%chunks != 0 {
 		min = fmt.Sprintf("1%s", strings.Repeat("0", n))
 		n++
 	}
-	// if the max is an odd number of digits, knock it down the highest x-1 digit value
-	if x%2 != 0 {
+	// if the max is not evenly divisible by chunks, knock it down the highest x-1 digit value
+	if x%chunks != 0 {
 		max = strings.Repeat("9", x-1)
 		x--
 	}
 
 	maxVal, _ := strconv.Atoi(max)
 	minVal, _ := strconv.Atoi(min)
-	testVal, _ := strconv.Atoi(min[:n/2])
+	testVal, _ := strconv.Atoi(min[:n/chunks])
 
-	// Split the id in half and begin incrementing that number
+	// Split the id in chunks and begin incrementing that number
 	for {
 		valStr := strconv.Itoa(testVal)
-		repeatVal, _ := strconv.Atoi(fmt.Sprintf("%s%s", valStr, valStr))
-		//. if the number created by repeating the half number is in range, add it to the list
+		repeatVal, _ := strconv.Atoi(strings.Repeat(valStr, chunks))
+
+		//. if the number created by repeating the chunk number is in range, add it to the list
 		if repeatVal <= maxVal && repeatVal >= minVal {
 			ids = append(ids, repeatVal)
 		}
